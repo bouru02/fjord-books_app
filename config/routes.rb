7125a009e7c57048e189e46bbfcd35a335447c1f
bouru02 <bouru02@gmail.com>
2022-07-27 14:21:05 +0900

    言語の切り替えをボタンでできるように変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '(:locale)', locale: /#{I18n.available_locales.map(&:to_s).join("|")}/ do
+    resources :books
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
