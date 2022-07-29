aecd88e79d00951a681501bf27ee26a532651546
bouru02 <bouru02@gmail.com>
2022-07-29 16:44:56 +0900

    ユーザー情報編集で郵便番号も変更可能に

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_out_path_for(resource) 
     books_path
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:zip_code])
   end
 end
