258cbdd5aaf2c89adc6a4b7a52cb71bd400f55d6
bouru02 <bouru02@gmail.com>
2022-09-12 14:39:47 +0900

    不要なメソッドを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  before_action :current_user_id, if: :user_signed_in?
 
   def after_sign_out_path_for(_resource)
     books_path
@@ -13,9 +12,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: %i[zip_code address introduction])
-  end
-
-  def current_user_id
-    @current_user_id = current_user.id
   end
 end
